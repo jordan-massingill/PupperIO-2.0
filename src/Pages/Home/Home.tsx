37e17e412b620523d29bb17979ac0ac105774451
Jordan Massingill <jgaylemass@gmail.com>
2023-03-07 04:16:23 +0000

    Update 5 files

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,3 +1,15 @@
-// const Home = () => {
-//   return <>hi</>;
-// };
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  color: green;
+`;
+
+const Home = () => {
+  return (
+    <div>
+      <header>Hello</header>
+    </div>
+  );
+};
+
+export default Home;
